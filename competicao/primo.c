52710202feed4628185b07ac39c27ceb5acccd0f
Rodolfo <rodolfoseki@yahoo.com.br>
2016-03-11 09:43:08 -0300

    adição competição de primos e projeto1

--- competicao/primo.c
+++ competicao/primo.c
@@ -1,15 +1,33 @@
 #include <stdio.h>
 #include <omp.h>
 #include <stdlib.h>
+#include <math.h>
 
 int primo(int n){
-  int i;
-
-  for(i = 2; i < n; i ++)
+  int i, j = sqrt(n);
+  
+  for(i = 2; i <= j; i++)
     if (n % i == 0)
       return 0;
-  
+
   return 1;
+}
+
+
+
+int trial(int n_primo){
+	int i,j,k;
+	
+	i =1;
+	j =2;
+	while(i < n_primo){
+		if(primo(++j)){
+			i++;
+		}
+	}
+	
+	
+	return j;
 }
 
 
@@ -17,9 +35,10 @@ int main(int argc, char *argv[])
 {
   int n = strtol(argv[1], NULL, 10);;
   
-  if(n ==2)
+  if(n==1)
 	printf("2\n");
-  else
-	printf("%d\n", primo(n));
+  else{
+	printf("%d\n", trial(n));
+  }
 }
 
